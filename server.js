7fb8403448fd30df66344a33170daec76d66085a
Vadivelu <vimal.khanna.vadivelu@sap.com>
2017-11-11 10:21:20 -0800

    adding functionalities to server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-app.get('/hello', function (req, res) {
+app.get('/watch', function (req, res) {
   var RequestURL='https://api.coinmarketcap.com/v1/ticker/';
   request(RequestURL, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body);
+    if (!error) {
+      var result = JSON.parse(body);
+      for(var i=0;i<result.length;i++) {
+        console.log(result[i].symbol + " " + result[i].price_usd);
+      }
     }
   })
 });
