7ac5d909fb70c5b74c3cdaac1858b61b9fe5e5da
Vadivelu <vimal.khanna.vadivelu@sap.com>
2017-12-20 15:51:51 -0800

    basic commit

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var express = require('express');
-var app = express();
-var request = require('request');
+const express = require('express');
+const app = express();
+const request = require('request');
+
 app.get('/watch', function (req, res) {
+  console.log("Hello World");
   var RequestURL='https://api.coinmarketcap.com/v1/ticker/';
   request(RequestURL, function (error, response, body) {
     if (!error) {
-      var result = JSON.parse(body);
-      for(var i=0;i<result.length;i++) {
-        console.log(result[i].symbol + " " + result[i].price_usd);
-      }
+      var parsedBody = JSON.parse(body);
+      res.send(parsedBody);
+      // for(var i=0;i<parsedBody.length;i++) {
+      //   console.log(parsedBody[i].name + " " + parsedBody[i].price_usd);
+      // }
     }
   })
 });
-app.listen(3000, function () {
-});
+
+app.listen(3000);
