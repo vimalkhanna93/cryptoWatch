1476b7f6cced889abea8d34cff8937b78b5e78f7
Vadivelu <vimal.khanna.vadivelu@sap.com>
2017-12-20 16:39:29 -0800

    adding fully functional search box

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const app = express();
 const request = require('request');
 
 app.get('/watch', function (req, res) {
-  console.log("Hello World");
+  var coinName = req.query.coin;
+  // console.log(coinName);
   var RequestURL='https://api.coinmarketcap.com/v1/ticker/';
   request(RequestURL, function (error, response, body) {
     if (!error) {
-      var parsedBody = JSON.parse(body);
-      res.send(parsedBody);
-      // for(var i=0;i<parsedBody.length;i++) {
-      //   console.log(parsedBody[i].name + " " + parsedBody[i].price_usd);
-      // }
+      var coinList = JSON.parse(body);
+      for(var i=0;i<coinList.length;i++) {
+        if(coinList[i].id == coinName) {
+          res.send(coinList[i]);
+        }
+      }
     }
   })
 });
