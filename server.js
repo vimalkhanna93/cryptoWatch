e1fcdcc923b39671b19ca74b7d87c0f4555e543f
Vadivelu <vimal.khanna.vadivelu@sap.com>
2018-01-11 10:53:57 -0800

    connected the storage to the application

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const request = require('request');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/user-details", function(err, db) {
+  if (err) throw err;
+  // console.log("Database created!");
+  db.createCollection("users", function(err, res) {
+    if (err) throw err;
+    // console.log("Collection created!");
+  });
+  db.close();
+});
 
 app.get('/watch', function (req, res) {
   var coinName = req.query.coin;
@@ -15,6 +29,16 @@ app.get('/watch', function (req, res) {
       }
     }
   })
+});
+
+app.post('/register', function (req, res) {
+  var user = req.body;
+  MongoClient.connect("mongodb://localhost:27017/user-details", function(err, db) {
+    db.collection("users").insertOne(user, function(err, res) {
+      if (err) throw err;
+      db.close();
+    });
+  });
 });
 
 app.listen(3000);
